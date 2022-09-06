1e0b03d695ee8899d851a843815af56b1c9f1765
only丶炀 <1763645020@qq.com>
2022-09-06 22:27:01 +0800

    style: format code

--- type-challenges/00189-easy-awaited/template.ts
+++ type-challenges/00189-easy-awaited/template.ts
@@ -1,1 +1,2 @@
-type MyAwaited<T extends Promise<any>> = T extends Promise<infer U> ? U extends Promise<any> ? MyAwaited<U> : U : never
+type MyAwaited<T extends Promise<any>> = T extends Promise<infer U>
+    ? U extends Promise<any> ? MyAwaited<U> : U : never
